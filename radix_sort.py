dd5efc3d9f90dc91fa0e624d05c3d0a08c79c33b
RafaelMorschbacher <rafa2002cunha@gmail.com>
2023-02-11 21:28:39 -0300

    Funcionando, ainda com vetor output separado

--- radix_sort.py
+++ radix_sort.py
@@ -22,7 +22,13 @@ def max_len(arr):
     return max_size
 #print(textToArray(frankestein))
 
+def getIndex(char):
+    char = char.upper()
+    return ord(char) - 64
+
 test = ['rafael', 'aaa', 'panela', 'computacao', 'computador', 'ar', 'bola', 'massa', 'zebra', 'paralelepipedo']
+test_2 = ['aba', '@bc', 'nan', 'ada']
+test_chars = ['a', 'b', 'z', 'g', 'j', 'i', 'c']
 
 def fill_words(arr):
     size = max_len(arr)
@@ -35,15 +41,42 @@ def fill_words(arr):
             filler_string = ''.join(filler)
             arr[index] = word + filler_string
 
-# Python program for implementation of Radix Sort
-# A function to do counting sort of arr[] according to
-# the digit represented by exp.
+
+#CODIGO INTERNET
+def countingSort(arr, charIndex):
+
+    arr_len = len(arr)
+    #Array de contagem
+    count = 27*[0]
+
+    for i in arr:
+        count[getIndex(i[charIndex])] += 1
+
+    # Array de acumulação
+    for i in range(1,27):
+        count[i] += count[i-1]
+
+    # Array output
+    output = arr_len*[0]
+    for i in reversed(range(0, arr_len)):
+        acc_pos = getIndex(arr[i][charIndex])
+        output_pos = count[acc_pos]
+
+        output[output_pos -1] = arr[i]
+        count[acc_pos] -= 1
+
+    return output
 
 
-
-
-
+def radixSort(arr):
+    for i in reversed(range(0, max_len(arr) - 1)):
+        arr = countingSort(frank_array, i)
 
 frank_array = textToArray(frankestein)
 fill_words(frank_array)
+
+for i in reversed(range(0,max_len(frank_array)-1)):
+    frank_array = countingSort(frank_array, i)
+
+#result = countingSort(frank_array,0)
 print(frank_array)
