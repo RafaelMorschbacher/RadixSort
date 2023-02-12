bda36548f5bdbe713cdad5b57627f8f65f26804a
RafaelMorschbacher <rafa2002cunha@gmail.com>
2023-02-11 22:42:56 -0300

    arquivos de saída funcionando

--- radix_sort.py
+++ radix_sort.py
@@ -1,82 +1,104 @@
 # ----------------- ARQUIVOS -----------------------
-# Leitura de arquivo de entrada -> cada vetor será inserido em lists
+# Leitura de arquivos de entrada e saída
 frankestein = open('frankestein_clean.txt', 'r')
+warAndPeace = open('war_and_peace_clean.txt', 'r')
+frankestein_output = open('frankenstein_ordenado.txt', 'w')
+war_peace_output = open('war_and_peace_ordenado.txt', 'w')
 
 
-
+# Funções Auxiliares
 def textToArray(file):
     read = file.readlines()
     words = []
     for line in read:
         cleanLine = line.strip().split()  # remove \n da linha
         for word in cleanLine:
-            if len(word)>=4:
+            if len(word) >= 4:
                 words.append(word)
+    fill_words(words)
     return words
 
+
+# Tamanho da maior palavra do array
 def max_len(arr):
     max_size = 0
     for word in arr:
-        if len(word)> max_size:
+        if len(word) > max_size:
             max_size = len(word)
     return max_size
-#print(textToArray(frankestein))
 
+
+# getIndex: char => posição (de 0 (@) até 26 (Z))
 def getIndex(char):
     char = char.upper()
     return ord(char) - 64
 
-test = ['rafael', 'aaa', 'panela', 'computacao', 'computador', 'ar', 'bola', 'massa', 'zebra', 'paralelepipedo']
-test_2 = ['aba', '@bc', 'nan', 'ada']
-test_chars = ['a', 'b', 'z', 'g', 'j', 'i', 'c']
 
+# Preeche palavras com @'s para um tamanho padrão (tamanho da maior palavra)
 def fill_words(arr):
     size = max_len(arr)
     for index, word in enumerate(arr):
         word_len = len(word)
         if word_len < size:
             filler = []
-            for i in range(word_len,size):
+            for i in range(word_len, size):
                 filler.append('@')
             filler_string = ''.join(filler)
             arr[index] = word + filler_string
 
 
-#CODIGO INTERNET
 def countingSort(arr, charIndex):
-
     arr_len = len(arr)
-    #Array de contagem
-    count = 27*[0]
+    # Array de contagem
+    count = 27 * [0]
 
     for i in arr:
         count[getIndex(i[charIndex])] += 1
 
     # Array de acumulação
-    for i in range(1,27):
-        count[i] += count[i-1]
+    for i in range(1, 27):
+        count[i] += count[i - 1]
 
     # Array output
-    output = arr_len*[0]
+    output = arr_len * [0]
     for i in reversed(range(0, arr_len)):
         acc_pos = getIndex(arr[i][charIndex])
         output_pos = count[acc_pos]
 
-        output[output_pos -1] = arr[i]
+        output[output_pos - 1] = arr[i]
         count[acc_pos] -= 1
 
-    return output
+    # arr = output
+    for i in range(0, arr_len):
+        arr[i] = output[i]
 
 
 def radixSort(arr):
     for i in reversed(range(0, max_len(arr) - 1)):
-        arr = countingSort(frank_array, i)
+        countingSort(arr, i)
+
+
+def output_write(arr, output_file):
+    arr_len = len(arr)
+    for i in range(0, arr_len):
+        if i == 0 or arr[i] != arr[i - 1]:
+            counter = 0
+            j = i
+            while arr[j] == arr[i] and j < arr_len-1:
+                counter += 1
+                j += 1
+
+            word = arr[i].replace('@', '')
+            output_file.write(word + ' ' + str(counter) + '\n')
+
+
+# Main:
+war_array = textToArray(warAndPeace)
+radixSort(war_array)
+output_write(war_array, war_peace_output)
 
 frank_array = textToArray(frankestein)
-fill_words(frank_array)
+radixSort(frank_array)
+output_write(frank_array, frankestein_output)
 
-for i in reversed(range(0,max_len(frank_array)-1)):
-    frank_array = countingSort(frank_array, i)
 
-#result = countingSort(frank_array,0)
-print(frank_array)
